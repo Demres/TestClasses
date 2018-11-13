715fea20a778a02fce98b49686ac8fbe8e67b398
Demres <bialy1423@gmail.com>
2018-11-13 09:59:29 +0100

    New Zadanie

---
+++ src/git/Zadanie.java
@@ -0,0 +1,15 @@
+package git;
+
+public class Zadanie {
+
+	String text = new String();
+	
+	public Zadanie(String text) {
+		this.text = text;
+	}
+	
+	public void show() {
+		System.out.println(text);
+	}
+}
+
